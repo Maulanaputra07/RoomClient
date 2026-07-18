f1df867efc1a417afca67ed79dec1acbaa318f5f
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2026-07-18 20:51:13 +0700

    design(songlist): justify songlist layout

--- RoomClient/Views/SongList/SongListView.xaml.cs
+++ RoomClient/Views/SongList/SongListView.xaml.cs
@@ -1,65 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RoomClient.Core.Models;
+using RoomClient.ViewModels;
 
 namespace RoomClient.Views.SongList
 {
-    /// <summary>
-    /// Interaction logic for SongListView.xaml
-    /// </summary>
     public partial class SongListView : UserControl
     {
         public Grid ContentGridControl => ContentGrid;
         public Button ToggleButtonControl => ToggleButton;
 
-        private bool _isExpanded = true;
         private bool _isCollapsed;
+        private ObservableCollection<Song>? _observedResults;
 
         public event EventHandler<bool>? ToggleRequested;
+
         public SongListView()
         {
             InitializeComponent();
             ToggleButton.Content = "▲ Hide";
+
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            DetachResultsCollection();
+
+            if (DataContext is MainWindowViewModel mainWindowViewModel)
+            {
+                AttachResultsCollection(mainWindowViewModel.Search.Results);
+            }
+        }
+
+        private void AttachResultsCollection(ObservableCollection<Song> results)
+        {
+            _observedResults = results;
+            _observedResults.CollectionChanged += OnResultsCollectionChanged;
+            ApplyAutoState(results.Count);
+        }
+
+        private void DetachResultsCollection()
+        {
+            if (_observedResults is not null)
+            {
+                _observedResults.CollectionChanged -= OnResultsCollectionChanged;
+                _observedResults = null;
+            }
+        }
+
+        private void OnResultsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_observedResults is not null)
+            {
+                ApplyAutoState(_observedResults.Count);
+            }
+        }
+
+        private void ApplyAutoState(int count)
+        {
+            _isCollapsed = count == 0;
+            ContentGrid.Visibility = _isCollapsed ? Visibility.Collapsed : Visibility.Visible;
+            ToggleButton.Content = _isCollapsed ? "▼ Show" : "▲ Hide";
         }
 
         private void OnToggleClick(object sender, RoutedEventArgs e)
         {
             _isCollapsed = !_isCollapsed;
-
-            ContentGrid.Visibility =
-                _isCollapsed
-                    ? Visibility.Collapsed
-                    : Visibility.Visible;
-
-            ToggleButton.Content =
-                _isCollapsed
-                    ? "▼ Show"
-                    : "▲ Hide";
-
+            ContentGrid.Visibility = _isCollapsed ? Visibility.Collapsed : Visibility.Visible;
+            ToggleButton.Content = _isCollapsed ? "▼ Show" : "▲ Hide";
             ToggleRequested?.Invoke(this, _isCollapsed);
         }
-
-        //private void OnToggleClick(object sender, RoutedEventArgs e)
-        //{
-        //    ToggleRequested?.Invoke(this, EventArgs.Empty);
-        //}
-
-        //private void OnToggleClick(object sender, RoutedEventArgs e)
-        //{
-        //    _isExpanded = !_isExpanded;
-        //    ContentGrid.Visibility = _isExpanded ? Visibility.Visible : Visibility.Collapsed;
-        //    ToggleButton.Content = _isExpanded ? "▲ Hide" : "▼ Show";
-        //}
     }
 }
